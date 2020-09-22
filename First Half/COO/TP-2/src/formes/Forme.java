a7f2f40d0c7adf5485fa6b1fb59672d9d6c96fe2
Calvin M.T <calvin.massonnet@gmail.com>
2020-09-22 06:24:08 +0200

    [COO] Add Forme

---
+++ First Half/COO/TP-2/src/formes/Forme.java
@@ -0,0 +1,31 @@
+package formes;
+
+public abstract class Forme {
+	
+	protected MachineTrace machineTrace;
+	
+	protected int xCentre = 0;
+	protected int yCentre = 0;
+	
+	protected int taille = 0;
+	
+	
+	
+	public Forme (MachineTrace m) {
+		machineTrace = m;
+	}
+	
+	
+	
+	public void fixerPosition (int x, int y) {
+		xCentre = x;
+		yCentre = y;
+	}
+	
+	public void fixerTaille (int t) {
+		taille = t;
+	}
+	
+	public abstract void dessiner ();
+	
+}
