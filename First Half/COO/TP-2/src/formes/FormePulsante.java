3485bba1c27b1da6dfb81f5c92361d23f3e5acd9
Calvin M.T <calvin.massonnet@gmail.com>
2020-09-22 13:27:26 +0200

    [COO] Add FormePulsante

---
+++ First Half/COO/TP-2/src/formes/FormePulsante.java
@@ -0,0 +1,24 @@
+package formes;
+
+public abstract class FormePulsante extends Forme {
+	
+	private int etape = 0;
+	private int amplitudePulsation = 20;
+	
+	
+	
+	public FormePulsante (MachineTrace m) {
+		super(m);
+	}
+	
+	
+	
+	public int etapeSuivante () {
+		etape++;
+		if (etape > FormesPulsantes.etapesPulsations) {
+			etape = 0;
+		}
+		return (int) (amplitudePulsation * (Math.sin(etape * 2 * Math.PI / FormesPulsantes.etapesPulsations) + 1) / 2);
+	}
+	
+}
