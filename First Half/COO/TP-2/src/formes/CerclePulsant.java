d06bdfe7261ba141cfa2bc42ef01480da470e292
Calvin M.T <calvin.massonnet@gmail.com>
2020-09-23 19:02:14 +0200

    [COO] Fix Cercle pulsating in its center

--- First Half/COO/TP-2/src/formes/CerclePulsant.java
+++ First Half/COO/TP-2/src/formes/CerclePulsant.java
@@ -10,13 +10,14 @@ public class CerclePulsant extends FormePulsante {
 	
 	@Override
 	public void dessiner () {
-		int y = yCentre - (taille / 2);
+		double y = yCentre + (taille / 2);
 		machineTrace.placer(xCentre, y);
 		machineTrace.baisser();
+		machineTrace.orienter(180);
 		double circonference = Math.PI * taille;
 		for (int i=0; i<360; i++) {
-			machineTrace.tournerGauche(1);
 			machineTrace.avancer(circonference/360);
+			machineTrace.tournerGauche(1);
 		}
 		machineTrace.lever();
 	}
