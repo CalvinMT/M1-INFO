bb2b9e20ef54cb5db7b6070b3a4fc25293ab5d19
Calvin M.T <calvin.massonnet@gmail.com>
2020-09-27 13:06:27 +0200

    [TLI] Create actions to add & remove functions

---
+++ First Half/TLI/TP-1/src/grapher/ui/ActionAddFunction.java
@@ -0,0 +1,66 @@
+package grapher.ui;
+
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.AbstractAction;
+import javax.swing.JComponent;
+import javax.swing.JOptionPane;
+
+public class ActionAddFunction extends AbstractAction implements FunctionListListener {
+	
+	JComponent parent;
+	
+	private List <ToolListener> listeners = new ArrayList<>();
+	
+	
+	
+	public ActionAddFunction (JComponent parent, String text) {
+		super(text);
+		this.parent = parent;
+	}
+	
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String newExpression = (String)JOptionPane.showInputDialog(
+                parent,
+                "New expression:",
+                "Add expression",
+                JOptionPane.PLAIN_MESSAGE,
+                null,
+                null,
+                "");
+		if (newExpression != null  &&  newExpression.length() > 0) {
+			for (ToolListener listener : listeners) {
+				listener.onFunctionAdd(newExpression);
+			}
+		}
+	}
+	
+	
+	
+	public void addListener (ToolListener listener) {
+		listeners.add(listener);
+	}
+	
+	
+	
+	@Override
+	public void onFunctionSelection(int selected, String name) {
+		return;
+	}
+	
+	@Override
+	public void onFunctionAdd(String function) {
+		return;
+	}
+	
+	@Override
+	public void onFunctionRemove(int function) {
+		return;
+	}
+	
+}
---
+++ First Half/TLI/TP-1/src/grapher/ui/ActionRemoveFunction.java
@@ -0,0 +1,74 @@
+package grapher.ui;
+
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.AbstractAction;
+import javax.swing.JComponent;
+import javax.swing.JOptionPane;
+
+public class ActionRemoveFunction extends AbstractAction implements FunctionListListener {
+	
+	JComponent parent;
+	
+	private List <ToolListener> listeners = new ArrayList<>();
+
+	private int selectedFunction = -1;
+	private String selectedFunctionName = "";
+	
+	
+	
+	public ActionRemoveFunction (JComponent parent, String text) {
+		super(text);
+		this.parent = parent;
+	}
+	
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (selectedFunction >= 0) {
+			Object[] options = {"Remove", "Cancel"};
+			int answer = JOptionPane.showOptionDialog(
+					parent,
+					"Do you want to remove " + selectedFunctionName + "? This action cannot be undone.",
+					"Remove expression",
+					JOptionPane.YES_NO_OPTION,
+					JOptionPane.WARNING_MESSAGE,
+					null,
+					options,
+					options[0]);
+			if (answer == 0) {
+				for (ToolListener listener : listeners) {
+					listener.onFunctionRemove(selectedFunction);
+				}
+			}
+		}
+	}
+	
+	
+	
+	public void addListener (ToolListener listener) {
+		listeners.add(listener);
+	}
+	
+	
+	
+	@Override
+	public void onFunctionSelection(int selected, String name) {
+		selectedFunction = selected;
+		selectedFunctionName = name;
+	}
+	
+	@Override
+	public void onFunctionAdd(String function) {
+		return;
+	}
+	
+	@Override
+	public void onFunctionRemove(int function) {
+		return;
+	}
+	
+}
