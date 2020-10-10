47dae5e9ba4674d1dbc2aa27d89d0e2c88ceb40a
Calvin M.T <calvin.massonnet@gmail.com>
2020-10-10 23:25:57 +0200

    [BD] Add changeCageFunction()

--- First Half/BD/TP-2/src/Fonctionnalites.java
+++ First Half/BD/TP-2/src/Fonctionnalites.java
@@ -13,4 +13,29 @@ public class Fonctionnalites {
 		this.connection = c;
 	}
 	
+	
+	
+	/**
+	 * 
+	 * @return
+	 * @throws SQLException
+	 */
+	public int changeCageFunction () throws SQLException {
+		int nbStatementChanged = 0;
+		String cage = "";
+		String fonction = "";
+		System.out.println(" - Changer la fonction d'une cage - ");
+		System.out.print("Cage : ");
+        cage = LectureClavier.lireChaine();
+		System.out.print("Nouvelle fonction : ");
+        fonction = LectureClavier.lireChaine();
+        
+        Statement statement = connection.createStatement();
+        nbStatementChanged = statement.executeUpdate(""
+        		+ "UPDATE LesCages "
+        		+ "SET fonction = '" + fonction + "' "
+				+ "WHERE noCage = " + Integer.parseInt(cage) + " ");
+        statement.close();
+        return nbStatementChanged;
+	}
 }
