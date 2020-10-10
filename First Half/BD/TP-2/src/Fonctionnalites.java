d25b2086861392697fbb28ffcc5c981db82dc947
Calvin M.T <calvin.massonnet@gmail.com>
2020-10-10 23:30:34 +0200

    [BD] Add changeGuardianAffectation()

--- First Half/BD/TP-2/src/Fonctionnalites.java
+++ First Half/BD/TP-2/src/Fonctionnalites.java
@@ -38,4 +38,69 @@ public class Fonctionnalites {
         statement.close();
         return nbStatementChanged;
 	}
+	
+	/**
+	 * 
+	 * @throws SQLException
+	 */
+	public void changeGuardianAffectation () throws SQLException {
+		String guardian = "";
+		String cageToWithdraw = "";
+		String cageToAdd = "";
+		System.out.println(" - Changer la fonction d'un gardien - ");
+		System.out.print("Nom du gardien : ");
+		guardian = LectureClavier.lireChaine();
+		
+		System.out.println("Cages gard�es par le gardien " + guardian + " : ");
+		Statement statement = connection.createStatement();
+		ResultSet guardianCages = statement.executeQuery(""
+				+ "SELECT noCage "
+				+ "FROM LesGardiens "
+				+ "WHERE nomE = '" + guardian + "' ");
+		while (guardianCages.next()) {
+			System.out.println("	- " + guardianCages.getString(1));
+		}
+		System.out.println();
+		statement.close();
+		
+		System.out.print("Cage � retirer de la fonction du gardien " + guardian + " : ");
+		cageToWithdraw = LectureClavier.lireChaine();
+		System.out.println();
+		statement = connection.createStatement();
+		statement.executeUpdate(""
+				+ "DELETE "
+				+ "FROM LesGardiens "
+				+ "WHERE nomE = '" + guardian + "' AND noCage = " + cageToWithdraw + " ");
+		statement.close();
+		
+		System.out.println("Cages dont la sp�cialit� est compatible avec le gardien " + guardian + " : ");
+		statement = connection.createStatement();
+		ResultSet guardianSpecialtySuggestion = statement.executeQuery(""
+				+ "SELECT A.noCage, fonction "
+				+ "FROM ( "
+		    		+ "SELECT noCage, fonction "
+		    		+ "FROM LesCages C "
+		    		+ "INNER JOIN LesSpecialites S "
+		    		+ "ON C.fonction = S.fonction_cage "
+		    		+ "WHERE S.nomE = '" + guardian + "' "
+				+ ") A "
+		    	
+				+ "LEFT JOIN LesGardiens G "
+				+ "ON A.noCage = G.noCage "
+				+ "WHERE G.noCage IS NULL");
+		while (guardianSpecialtySuggestion.next()) {
+			System.out.println("	- " + guardianSpecialtySuggestion.getString(1));
+		}
+		System.out.println();
+		statement.close();
+		
+		System.out.print("Quelle cage attribuer au gardien " + guardian + " : ");
+		cageToAdd = LectureClavier.lireChaine();
+		System.out.println();
+		statement = connection.createStatement();
+		statement.executeUpdate(""
+				+ "INSERT INTO LesGardiens "
+				+ "VALUES (" + cageToAdd + ", '" + guardian + "')");
+		statement.close();
+	}
 }
