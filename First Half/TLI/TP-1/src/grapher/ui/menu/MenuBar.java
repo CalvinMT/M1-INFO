db0eb4ded05adf41eec1f5cbc603dd18c5551bee
Calvin M.T <calvin.massonnet@gmail.com>
2020-10-11 20:45:01 +0200

    [TLI] Add function edit menu item

--- First Half/TLI/TP-1/src/grapher/ui/menu/MenuBar.java
+++ First Half/TLI/TP-1/src/grapher/ui/menu/MenuBar.java
@@ -6,6 +6,7 @@ import javax.swing.JMenuItem;
 
 import grapher.ui.ViewModeListener;
 import grapher.ui.actions.ActionAddFunction;
+import grapher.ui.actions.ActionEditFunction;
 import grapher.ui.actions.ActionRemoveFunction;
 import grapher.ui.actions.ActionViewModeList;
 import grapher.ui.actions.ActionViewModeTable;
@@ -15,6 +16,7 @@ public class MenuBar extends JMenuBar {
 
 	JMenu menuFunction;
 	ActionAddFunction actionAddFunction;
+	ActionEditFunction actionEditFunction;
 	ActionRemoveFunction actionRemoveFunction;
 	
 	JMenu menuWindow;
@@ -35,12 +37,15 @@ public class MenuBar extends JMenuBar {
 		menuFunction = new JMenu("Function");
 
 		actionAddFunction = new ActionAddFunction(this, "Add...");
+		actionEditFunction = new ActionEditFunction(this, "Edit...");
 		actionRemoveFunction = new ActionRemoveFunction(this, "Remove...");
 
 		JMenuItem itemAddFunction = new JMenuItem(actionAddFunction);
+		JMenuItem itemEditFunction = new JMenuItem(actionEditFunction);
 		JMenuItem itemRemoveFunction = new JMenuItem(actionRemoveFunction);
 
 		menuFunction.add(itemAddFunction);
+		menuFunction.add(itemEditFunction);
 		menuFunction.add(itemRemoveFunction);
 		
 		add(menuFunction);
@@ -78,6 +83,7 @@ public class MenuBar extends JMenuBar {
 	
 	public void addToolListener (ToolListener listener) {
 		actionAddFunction.addListener(listener);
+		actionEditFunction.addListener(listener);
 		actionRemoveFunction.addListener(listener);
 	}
 	
@@ -86,10 +92,14 @@ public class MenuBar extends JMenuBar {
 		actionViewModeTable.addListener(listener);
 	}
 	
-	
+
 	
 	public ActionRemoveFunction getActionRemoveFunction () {
 		return actionRemoveFunction;
+	}
+	
+	public ActionEditFunction getActionEditFunction () {
+		return actionEditFunction;
 	}
 	
 }
