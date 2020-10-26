dcd3a5a2042a039738b6593c8c61335d2452368e
Calvin M.T <calvin.massonnet@gmail.com>
2020-10-26 19:08:37 +0100

    [TLI] Remove unused imports

--- First Half/TLI/TP-1/src/grapher/ui/FunctionTableMouseAdapter.java
+++ First Half/TLI/TP-1/src/grapher/ui/FunctionTableMouseAdapter.java
@@ -3,13 +3,11 @@ package grapher.ui;
 import java.awt.Color;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JColorChooser;
 import javax.swing.JTable;
-import javax.swing.table.DefaultTableModel;
 
 public class FunctionTableMouseAdapter extends MouseAdapter {
 	
