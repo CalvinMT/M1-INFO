530b8e55a5cdc9e5f20b5372798b75060a5516fa
Calvin M.T <calvin.massonnet@gmail.com>
2020-10-28 22:38:30 +0100

    [TLI] Add maximum size & text font to toolbar buttons

--- First Half/TLI/TP-1/src/grapher/ui/tool/ToolBar.java
+++ First Half/TLI/TP-1/src/grapher/ui/tool/ToolBar.java
@@ -1,11 +1,17 @@
 package grapher.ui.tool;
 
+import java.awt.Dimension;
+import java.awt.Font;
+
 import javax.swing.JButton;
 import javax.swing.JToolBar;
 
 import grapher.ui.actions.Actions;
 
 public class ToolBar extends JToolBar {
+	
+	private static final Dimension BUTTON_SIZE = new Dimension(30, 30);
+	private static final Font BUTTON_TEXT_FONT = new Font("", Font.PLAIN, 18);
 	
 	private JButton buttonAdd;
 	private JButton buttonRemove;
@@ -24,10 +30,25 @@ public class ToolBar extends JToolBar {
 		buttonAdd.setText("+");
 		buttonRemove.setText("-");
 		buttonEdit.setText("✎");
+
+		buttonAdd.setFont(BUTTON_TEXT_FONT);
+		buttonRemove.setFont(BUTTON_TEXT_FONT);
+		buttonEdit.setFont(BUTTON_TEXT_FONT);
+		
+		setButtonSize(buttonAdd);
+		setButtonSize(buttonRemove);
+		setButtonSize(buttonEdit);
 		
 		add(buttonAdd);
 		add(buttonRemove);
 		add(buttonEdit);
+	}
+	
+	
+	
+	private void setButtonSize (JButton button) {
+		button.setPreferredSize(BUTTON_SIZE);
+		button.setMaximumSize(BUTTON_SIZE);
 	}
 	
 }
