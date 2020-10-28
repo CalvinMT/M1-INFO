6ba9ceac0007ed7e11e4e4e08ecc19551c90e33f
Calvin M.T <calvin.massonnet@gmail.com>
2020-10-28 22:18:37 +0100

    [TLI] Feature edit button in toolbar

--- First Half/TLI/TP-1/src/grapher/ui/tool/ToolBar.java
+++ First Half/TLI/TP-1/src/grapher/ui/tool/ToolBar.java
@@ -9,21 +9,25 @@ public class ToolBar extends JToolBar {
 	
 	private JButton buttonAdd;
 	private JButton buttonRemove;
+	private JButton buttonEdit;
 	
 	
 	
 	public ToolBar () {
 		super();
 		setFloatable(false);
-
+		
 		buttonAdd = new JButton(Actions.getInstance().actionAddFunction);
 		buttonRemove = new JButton(Actions.getInstance().actionRemoveFunction);
-
+		buttonEdit = new JButton(Actions.getInstance().actionEditFunction);
+		
 		buttonAdd.setText("+");
 		buttonRemove.setText("-");
+		buttonEdit.setText("✎");
 		
 		add(buttonAdd);
 		add(buttonRemove);
+		add(buttonEdit);
 	}
 	
 }
