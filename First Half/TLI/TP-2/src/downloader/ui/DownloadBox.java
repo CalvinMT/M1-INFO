fc0e7692a69b5911bd59b612d42fb4935f6b3629
Calvin M.T <calvin.massonnet@gmail.com>
2020-11-10 17:13:26 +0100

    [TLI] Add button to download box

--- First Half/TLI/TP-2/src/downloader/ui/DownloadBox.java
+++ First Half/TLI/TP-2/src/downloader/ui/DownloadBox.java
@@ -1,5 +1,8 @@
 package downloader.ui;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 
@@ -8,18 +11,25 @@ public class DownloadBox extends JPanel {
 	private final int index;
 	
 	private JProgressBar progressBar;
+	private JButton buttonChangeDownloadState;
 	
 	
 	
 	public DownloadBox (int boxIndex) {
 		this.index = boxIndex;
 		
+		BorderLayout borderLayout = new BorderLayout();
+		setLayout(borderLayout);
+		
 		progressBar = new JProgressBar(0, 100);
-		progressBar.setBounds(0, 0, 160, 30);
 		progressBar.setValue(0);
 		progressBar.setStringPainted(true);
 		
-		add(progressBar);
+		buttonChangeDownloadState = new JButton();
+		buttonChangeDownloadState.setText("❙❙");
+		
+		add(progressBar, BorderLayout.CENTER);
+		add(buttonChangeDownloadState, BorderLayout.EAST);
 	}
 	
 
