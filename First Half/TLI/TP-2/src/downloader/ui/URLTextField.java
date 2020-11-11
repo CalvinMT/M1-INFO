73c1810602113ea28ff09b48dc6089375e2daf3e
Calvin M.T <calvin.massonnet@gmail.com>
2020-11-11 11:09:49 +0100

    [TLI] Feature ghost text in URL text field

--- First Half/TLI/TP-2/src/downloader/ui/URLTextField.java
+++ First Half/TLI/TP-2/src/downloader/ui/URLTextField.java
@@ -1,5 +1,8 @@
 package downloader.ui;
 
+import java.awt.Color;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
@@ -10,17 +13,41 @@ import downloader.fc.actions.ActionAddDownloadListener;
 
 public class URLTextField extends JTextField implements ActionAddDownloadListener {
 	
+	private static final String DEFAULT_GHOST_TEXT = "https://...";
+	
+	private static Color DEFAULT_TEXT_COLOR;
+	private static final Color DEFAULT_GHOST_TEXT_COLOR = new Color(0, 0, 0, 100);
+	
+	
+	
 	public URLTextField () {
-		super("https://...");
+		DEFAULT_TEXT_COLOR = getSelectedTextColor();
 		
-		// TODO - in focus, setText("");
-		// TODO - out of focus, setText("https://...");
+		addFocusListener(new FocusListener() {
+			
+			@Override
+			public void focusLost(FocusEvent e) {
+				if (getText().trim().equals("")) {
+					setForeground(DEFAULT_GHOST_TEXT_COLOR);
+					setText(DEFAULT_GHOST_TEXT);
+				}
+			}
+			
+			@Override
+			public void focusGained(FocusEvent e) {
+				if (getText().trim().equals(DEFAULT_GHOST_TEXT)) {
+					setForeground(DEFAULT_TEXT_COLOR);
+					setText("");
+				}
+			}
+			
+		});
 		
-		this.addKeyListener(new KeyListener() {
+		addKeyListener(new KeyListener() {
 			
 			@Override
 			public void keyTyped(KeyEvent e) {
-				if (e.getExtendedKeyCode() == KeyEvent.VK_ENTER  &&  isFocusOwner()) {
+				if (e.getExtendedKeyCode() == KeyEvent.VK_ENTER) {
 					addURL();
 				}
 			}
