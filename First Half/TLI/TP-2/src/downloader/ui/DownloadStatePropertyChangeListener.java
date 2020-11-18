161c3965c256db64c91dbe632eda189666effe41
Calvin M.T <calvin.massonnet@gmail.com>
2020-11-18 12:43:19 +0100

    [TLI] Feature taskbar flash notification upon finished download

--- First Half/TLI/TP-2/src/downloader/ui/DownloadStatePropertyChangeListener.java
+++ First Half/TLI/TP-2/src/downloader/ui/DownloadStatePropertyChangeListener.java
@@ -3,6 +3,9 @@ package downloader.ui;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
 import downloader.fc.DownloadState;
 
 public class DownloadStatePropertyChangeListener implements PropertyChangeListener {
@@ -21,6 +24,7 @@ public class DownloadStatePropertyChangeListener implements PropertyChangeListen
 	public void propertyChange(PropertyChangeEvent e) {
 		if (e.getPropertyName().equals("download_state")) {
 			downloadBox.updateButtonState(DownloadState.valueOf((String) e.getNewValue().toString()));
+			((JFrame) SwingUtilities.getWindowAncestor(downloadBox)).toFront();
 		}
 	}
 	
